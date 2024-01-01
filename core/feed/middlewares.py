05c744d1db40f2325f96eb98cd424414ad3f4758
w0rng <work@w0rng.ru>
2024-01-01 21:29:21 +0700

    fix UserIdMiddleware

--- core/feed/middlewares.py
+++ core/feed/middlewares.py
@@ -1,14 +1,29 @@
 from random import choices
 from string import ascii_letters, digits
+from django.core.signing import Signer, BadSignature
 
 
 class UserIdMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.signer = Signer()
 
     def __call__(self, request):
         new_cookie = "".join(choices(ascii_letters + digits, k=8))
-        request.user_id = request.COOKIES.get("userID", new_cookie)
+
+        if request.user.is_authenticated:
+            request.user_id = request.user.id
+        elif "userID" in request.COOKIES:
+            try:
+                request.user_id = self.signer.unsign(request.COOKIES["userID"])
+            except BadSignature:
+                request.user_id = new_cookie
+        else:
+            request.user_id = new_cookie
+
         response = self.get_response(request)
-        response.set_cookie("userID", request.user_id, max_age=60 * 60 * 24 * 365 * 2)
+
+        signed_cookie = self.signer.sign(request.user_id)
+        response.set_cookie("userID", signed_cookie, max_age=60 * 60 * 24 * 365 * 2)
+
         return response
